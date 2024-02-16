41d84381d9928bb78e54bf59e9b2552968f88dc3
SarunasT <sarunas.tarvydas@hotmail.com>
2024-02-16 19:11:01 +0100

    basick types

--- first.js
+++ first.js
@@ -1,2 +1,17 @@
+"use strict";
+// basic types
+Object.defineProperty(exports, "__esModule", { value: true });
+// number, string, boolean, null, undefined, void
 var num = 5;
-console.log('Hello TS', num);
+num = 10;
+num = 'asd';
+var str = 'James';
+str = true;
+var isAdmin = true;
+isAdmin = false;
+isAdmin = 0;
+isAdmin = !!0;
+isAdmin = Boolean(' ');
+isAdmin = Boolean('0');
+isAdmin = Boolean(0);
+console.log('Hello TS', num, isAdmin);
--- first.ts
+++ first.ts
@@ -1,4 +1,32 @@
-let num = 5
 
-console.log('Hello TS', num );
+// basic types
 
+// number, string, boolean, null, undefined, void
+
+let num = 5;
+
+num = 10;
+
+num = 'asd';
+
+let str = 'James';
+
+str = true;
+
+let isAdmin: boolean = true;
+
+isAdmin = false;
+
+isAdmin = 0
+
+isAdmin = !!0
+
+isAdmin = Boolean(' ')
+
+isAdmin = Boolean('0')
+
+isAdmin = Boolean(0)
+
+console.log('Hello TS', num, isAdmin );
+
+export {}
