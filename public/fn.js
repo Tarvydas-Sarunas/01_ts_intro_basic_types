cf842ce872052a8129cf976cddc8730847885f49
SarunasT <sarunas.tarvydas@hotmail.com>
2024-02-17 13:13:32 +0100

    prompt and

---
+++ public/fn.js
@@ -0,0 +1,32 @@
+'use strict';
+console.log('fn.js file was loaded');
+// function
+function greet(name, weekDay) {
+    console.log(' hello', name, weekDay);
+}
+greet('James', 2);
+// greet('James')
+function sum(sk1, sk2) {
+    return sk1 + sk2;
+}
+// const sk1 = Number(prompt('ivesk sk 1'))
+// const sk2 = Number(prompt('ivesk sk 2'))
+// const sept = sum(sk1, sk2)
+// console.log('sept ===', sept);
+// const sk3 = Number(prompt('ivesk sk 1'))
+// const sk4 = Number(prompt('ivesk sk 2'))
+// function flow(): void {
+//   if (sk1 === null || sk2 === null) {
+//   return
+// }
+// sum(+sk1, +sk2)
+// }
+// flow()
+// const aha = sum(sk3, sk4)
+// console.log('sept ===', aha)
+function makePrice(value) {
+    const rez = `$ ${value.toFixed(2)} `;
+    console.log('rez ===', rez);
+    return rez;
+}
+makePrice(100);
---
+++ src/fn.ts
@@ -0,0 +1,46 @@
+'use strict';
+console.log('fn.js file was loaded');
+
+// function
+
+function greet(name: string, weekDay: number): void {
+  console.log(' hello', name, weekDay);
+}
+
+greet('James', 2)
+// greet('James')
+
+function sum(sk1: number, sk2: number): number {
+  return sk1 + sk2
+}
+
+// const sk1 = Number(prompt('ivesk sk 1'))
+// const sk2 = Number(prompt('ivesk sk 2'))
+
+
+// const sept = sum(sk1, sk2)
+// console.log('sept ===', sept);
+
+
+// const sk3 = Number(prompt('ivesk sk 1'))
+// const sk4 = Number(prompt('ivesk sk 2'))
+
+
+// function flow(): void {
+//   if (sk1 === null || sk2 === null) {
+//   return
+// }
+// sum(+sk1, +sk2)
+// }
+// flow()
+
+// const aha = sum(sk3, sk4)
+// console.log('sept ===', aha)
+
+function makePrice(value: number): string  {
+  const rez: string = `$ ${value.toFixed(2)} `
+  console.log('rez ===', rez);
+  return rez  
+
+}
+makePrice(100)
